f6a9acfe9c3eef3d78061d1f12f53a903e9fe6ce
hanna2100 <hanna0497@naver.com>
2019-12-13 15:52:55 +0900

    장한나림 UI 애니메이션추가

--- app/src/main/java/com/example/user/moviediary/util/DiaryDB.java
+++ app/src/main/java/com/example/user/moviediary/util/DiaryDB.java
@@ -30,7 +30,7 @@ public final class DiaryDB {
         public static final String CREATE_POSTING = "create table if not exists "
                 +POSTING_TBL+" ("
                 +MV_ID+" integer primary key, "
-                +MV_POSTER+" text not null, "
+                +MV_POSTER+" text , "
                 +TITLE+" text not null , "
                 +MV_DATE+" text not null , "
                 +POST_DATE+" text not null , "
@@ -48,6 +48,6 @@ public final class DiaryDB {
                 +LIKE_TBL+"("
                 +MV_ID+" integer primary key, "
                 +TITLE+" text not null, "
-                +MV_POSTER+" text not null);";
+                +MV_POSTER+" text );";
     }
 }
